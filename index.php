756e4169815a69363f5c3b6b3e6a082f6a741a60
Josh Rucker <ifiamblue@gmail.com>
2014-12-27 23:53:34 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,196 @@
+
+<!DOCTYPE html>
+<html prefix="og: http://ogp.me/ns#">
+
+<head>	
+
+	<title>365 Portraits :: Josh Rucker</title>
+	<link rel="stylesheet" type="text/css" href="_css/main.css" />
+
+	<meta charset="utf-8">
+	<meta content="General" name="rating"/>
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+    
+	<meta name="description" content="" />
+
+	<!-- Typekit -->
+    <script type="text/javascript" src="//use.typekit.net/meg6qsn.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	
+	<!--[if lt IE 9]>
+		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<?php /*
+
+	<meta name='robots' content='noindex,nofollow' />
+	<meta property="og:title" content="Hello world!" />
+	<meta property="og:type" content="article" />
+	<meta property="og:description" content="" />
+	<meta property="og:image" content="http://localhost:8888/joshrucker/wp-content/uploads/2014/09/8603684447_1d0284a771_o-650x430.jpg"/>
+	<meta property="og:url" content="http://localhost:8888/joshrucker/?p=1" />
+	<meta property="og:site_name" content="Josh Rucker"/>
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="@ifiamblue" />
+	<meta name="twitter:creator" content="@ifiamblue" />
+
+	*/ ?>
+
+</head>
+
+<body>
+		
+	<header id="header" role="banner">
+	
+		<div class="wrapper">
+		
+            <h1 class="heading-branding">
+				<a href="./">365 Portraits<b> / Josh Rucker</b></a>
+			</h1>
+			
+		</div>
+		
+   	</header><!-- end #header -->
+
+	<aside class="section about" role="complementary" aria-label="About the Author">
+
+		<div class="wrapper">
+
+			<p>I’m a front-end web developer at Interactive Intelligence in Durham, NC.  This site is a collection
+				of thoughts on web development, life, and the things that happen in 
+				between.  All thoughts and opinions expressed here are my own. <a class="more" href="http://localhost:8888/joshrucker/about/">Read&nbsp;more about me &raquo;</a></p>
+
+		</div>
+
+	</aside>
+
+	<section id="main" role="main">
+		
+		<div class="wrapper">
+
+			<article class="article">
+
+				<div class="pagination">
+			
+					<a class="previous-image" href="#prev">&laquo; Previous</a>
+					<a class="next-image" href="#next">Next &raquo;</a>
+
+				</div>
+				
+				<figure class="feature-image">
+
+					<img src="_img/portraits/1.jpg" srcset="_img/portraits/1-sm.jpg 1x, _img/portraits/1-med.jpg 1.5x, _img/portraits/1.jpg 2x" alt="Cody Wilcox" />
+
+				</figure>
+
+				<h2 class="article-heading"><span class="count">2</span> / Cody Wilcox</h2>		
+				<p class="article-meta">
+					<time class="article-timestamp" datetime="2014-09-11"> January 2, 2014</time> &mdash; Professional Kid
+				</p>
+
+			</article>
+		
+		</div><!-- end .wrapper -->
+		
+	</section><!--#main-->
+
+	<section class="section days">
+	
+		<div class="wrapper">
+		
+			<h2>Day:</h2>
+
+			<ul>
+				<li class="active"><a href="#">1</a></li>
+				<li><a href="#">2</a></li>
+				<li><a href="#">3</a></li>
+				<li><a href="#">4</a></li>
+				<li><a href="#">5</a></li>
+				<li><a href="#">6</a></li>
+				<li><a href="#">7</a></li>
+				<li><a href="#">8</a></li>
+				<li><a href="#">9</a></li>
+				<li><a href="#">10</a></li>
+				<li><a href="#">11</a></li>
+				<li><a href="#">12</a></li>
+				<li><a href="#">13</a></li>
+				<li><a href="#">14</a></li>
+				<li><a href="#">15</a></li>
+				<li><a href="#">16</a></li>
+				<li><a href="#">17</a></li>
+				<li><a href="#">18</a></li>
+				<li><a href="#">19</a></li>
+				<li><a href="#">20</a></li>
+				<li><a href="#">21</a></li>
+				<li><a href="#">22</a></li>
+				<li><a href="#">23</a></li>
+				<li><a href="#">24</a></li>
+				<li><a href="#">25</a></li>
+				<li><a href="#">26</a></li>
+				<li><a href="#">27</a></li>
+				<li><a href="#">28</a></li>
+				<li><a href="#">29</a></li>
+				<li><a href="#">30</a></li>
+				<li><a href="#">31</a></li>
+			</ul>
+		
+		</div><!-- end .wrapper -->
+	
+	</section><!-- end .section -->	
+
+	<section class="section months">
+	
+		<div class="wrapper">
+
+			<h2>Month:</h2>
+		
+			<ul>
+				<li class="active"><a href="#">Jan</a></li>
+				<li><a href="#">Feb</a></li>
+				<li><a href="#">Mar</a></li>
+				<li><a href="#">Apr</a></li>
+				<li><a href="#">May</a></li>
+				<li><a href="#">Jun</a></li>
+				<li><a href="#">Jul</a></li>
+				<li><a href="#">Aug</a></li>
+				<li><a href="#">Sep</a></li>
+				<li><a href="#">Oct</a></li>
+				<li><a href="#">Nov</a></li>
+				<li><a href="#">Dec</a></li>
+			</ul>
+		
+		</div><!-- end .wrapper -->
+	
+	</section><!-- end .section -->	
+	
+	<footer id="footer" role="contentinfo">
+
+		<div class="wrapper">
+
+			<p>All content copyright &copy; 2013 <strong><a href="http://www.joshrucker.com">Josh Rucker</a></strong> unless otherwise attributed.</p>
+
+		</div>	
+
+	</footer>
+	
+	<?php /*
+
+	<script type="text/javascript">
+
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-6162050-9']);
+		_gaq.push(['_setDomainName', 'joshrucker.com']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+
+	</script>
+
+	*/ ?>
+
+</body>
+</html>
